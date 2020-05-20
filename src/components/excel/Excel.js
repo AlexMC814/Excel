4cf24737dedf4e4d89ba2b53093252b898e403f0
alexmusarov <alexmc814@gmail.com>
2020-05-20 16:58:03 +0300

    Finish framework

---
+++ src/components/excel/Excel.js
@@ -0,0 +1,32 @@
+import dom from '@core/dom';
+
+class Excel {
+  constructor(selector, options) {
+    this.$el = dom(selector);
+    this.components = options.components || [];
+  }
+
+  getRoot() {
+    const $root = dom.create('div', 'excel');
+
+    this.components = this.components.map(Component => {
+      const $el = dom.create('div', Component.className);
+
+      const component = new Component($el);
+
+      $el.html(component.toHTML());
+      $root.append($el);
+      return component;
+    });
+
+    return $root;
+  }
+
+  render() {
+    this.$el.append(this.getRoot());
+
+    this.components.forEach(component => component.init());
+  }
+}
+
+export default Excel;
