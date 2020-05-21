c26f8d6fd2ad473cafb386cf696d01b134ecc3b2
alexmusarov <alexmc814@gmail.com>
2020-05-21 11:32:48 +0300

    Test commit

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -34,18 +34,15 @@ export function createTable(rowsCount = 15) {
   const colsCount = CODES.Z - CODES.A + 1;
   const rows = [];
   const cols = new Array(colsCount)
-      .fill('')
-      .map((_, index) => toChar(index))
-      .map(toColumn)
-      .join('');
+    .fill('')
+    .map((_, index) => toChar(index))
+    .map(toColumn)
+    .join('');
 
   rows.push(createRow(null, cols));
 
   for (let i = 0; i < rowsCount; i++) {
-    const cells = new Array(colsCount)
-        .fill('')
-        .map(toCell)
-        .join('');
+    const cells = new Array(colsCount).fill('').map(toCell).join('');
 
     rows.push(createRow(i + 1, cells));
   }
